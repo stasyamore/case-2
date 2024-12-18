15b6d85ddecf2783d977d04c5050259c1641e418
stasyamore <anastasiadobrozhan@yandex.ru>
2024-12-18 13:49:09 +0300

    Переделали структуру проекта

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
-import os
-from docx import Document
-from docx.shared import Pt
-from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
-from docx.oxml import OxmlElement
+## main стартовый модуль проекта
 
-# Функция для изменения формата документа
-def format_docx(file_path):
-    doc = Document(file_path)
-# Изменяем стиль каждого абзаца в документе
-    for paragraph in doc.paragraphs:
-        # Устанавливаем шрифт и размер шрифта
-        for run in paragraph.runs:
-            run.font.name = 'Times New Roman'
-            run.font.size = Pt(14)
- # Устанавливаем межстрочный интервал на 1.5
-        paragraph.paragraph_format.line_spacing = 1.5
-   # cохраняем изменения
-    doc.save(file_path)
+from functions import format_docx, change_format
+import os
+
+def main():
+    # TODO вызов функции change_format()
+
+    # TODO все что ниже переннести в change_format()
+
+    # Путь к папке с документами
+    folder_path = '/Users/dmitry.dobrozan/Desktop/case3'  
+    # Проходим по всем файлам в папке
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.docx'):
+            file_path = os.path.join(folder_path, filename)
+            format_docx(file_path)
+            print(f"Форматирование завершено для: {filename}")
+
+# инициализационный скрипт
+if __name__ == "__main__":
+    main()
     
-# Путь к папке с документами
-folder_path = 'path/to/your/documents'  
-# Проходим по всем файлам в папке
-for filename in os.listdir(folder_path):
-    if filename.endswith('.docx'):
-        file_path = os.path.join(folder_path, filename)
-        format_docx(file_path)
-        print(f"Форматирование завершено для: {filename}")
