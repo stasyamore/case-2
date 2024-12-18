8c2a8e941d0fe3954cdb336d450d8eb2031f6f49
stasyamore <anastasiadobrozhan@yandex.ru>
2024-12-18 12:50:52 +0300

    Путь к папке с документами

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ from docx import Document
 from docx.shared import Pt
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.oxml import OxmlElement
-
+# Изменяем стиль каждого абзаца в документе
+for paragraph in doc.paragraphs:
+        # Устанавливаем шрифт и размер шрифта
+    for run in paragraph.runs:
+        run.font.name = 'Times New Roman'
+        run.font.size = Pt(14)
+# Путь к папке с документами
+folder_path = 'path/to/your/documents'  
