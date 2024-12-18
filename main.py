6bec7c1b6e84ee75d2b476851bc721b70350cac8
Art007kz <Artaru11kz>
2024-12-18 12:38:51 +0300

    Изменяем стиль каждого абзаца в документе

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+import os
+from docx import Document
+from docx.shared import Pt
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.oxml import OxmlElement
 # Функция для изменения формата документа
 def format_docx(file_path):
     doc = Document(file_path)
+# Изменяем стиль каждого абзаца в документе
+    for paragraph in doc.paragraphs:
+        # Устанавливаем шрифт и размер шрифта
+        for run in paragraph.runs:
+            run.font.name = 'Times New Roman'
+            run.font.size = Pt(14)
+
