26c4edafbe76dab31ea3b18466b7e95891c8e5c2
stasyamore <anastasiadobrozhan@yandex.ru>
2024-12-20 10:54:18 +0300

    добавила todo

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-## main стартовый модуль проекта
-
 from functions import format_docx, change_format
 import os
 
+
 def main():
-    # TODO вызов функции change_format()
+    # Вызов функции change_format()
+    change_format('/Users/dmitry.dobrozan/Desktop/case3')
 
-    # TODO все что ниже переннести в change_format()
 
-    # Путь к папке с документами
-    folder_path = '/Users/dmitry.dobrozan/Desktop/case3'  
-    # Проходим по всем файлам в папке
+def change_format(folder_path):
+    # Проход по всем файлам в папке
     for filename in os.listdir(folder_path):
         if filename.endswith('.docx'):
             file_path = os.path.join(folder_path, filename)
             format_docx(file_path)
             print(f"Форматирование завершено для: {filename}")
 
-# инициализационный скрипт
+
+# Инициализация скрипта
 if __name__ == "__main__":
     main()
-    
