f49b59bf3e72e5871e49da4fc7074398b591acb2
stasyamore <anastasiadobrozhan@yandex.ru>
2024-12-20 14:17:58 +0300

    сохранила изменения

--- functions.py
+++ functions.py
@@ -1,12 +1,33 @@
-
 # functions модуль для импорта функций для решения задачи  
- 
-#TODO - добавить импорты внешних зависимостей  
-# import <название зависимости> 
- 
-def func1(): 
-    pass 
- 
- 
-def fucn2(): 
-    pass
+from docx import Document
+from docx.shared import Pt
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from docx.oxml import OxmlElement
+
+
+# Функция для изменения формата документа
+def format_docx(file_path):
+    doc = Document(file_path)
+    
+    # Изменяем стиль каждого абзаца в документе
+    for paragraph in doc.paragraphs:
+        # Устанавливаем шрифт и размер шрифта
+        for run in paragraph.runs:
+            font = run.font
+            font.name = 'Times New Roman'
+            font.size = Pt(14)
+        
+        # Устанавливаем межстрочный интервал на 1.5
+        paragraph.paragraph_format.line_spacing_rule = 1.5
+    
+    # Сохраняем изменения
+    doc.save(file_path)
+
+
+def change_format(folder_path):
+    # Проходим по всем файлам в папке
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.docx'):
+            file_path = os.path.join(folder_path, filename)
+            format_docx(file_path)
+            print(f"Форматирование завершено для: {filename}")
--- main.py
+++ main.py
@@ -1,28 +1,21 @@
+from functions import format_docx, change_format
 import os
-from docx import Document
-from docx.shared import Pt
-from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
-from docx.oxml import OxmlElement
 
-# Функция для изменения формата документа
-def format_docx(file_path):
-    doc = Document(file_path)
-# Изменяем стиль каждого абзаца в документе
-    for paragraph in doc.paragraphs:
-        # Устанавливаем шрифт и размер шрифта
-        for run in paragraph.runs:
-            run.font.name = 'Times New Roman'
-            run.font.size = Pt(14)
- # Устанавливаем межстрочный интервал на 1.5
-        paragraph.paragraph_format.line_spacing = 1.5
-   # cохраняем изменения
-    doc.save(file_path)
-    
-# Путь к папке с документами
-folder_path = 'path/to/your/documents'  
-# Проходим по всем файлам в папке
-for filename in os.listdir(folder_path):
-    if filename.endswith('.docx'):
-        file_path = os.path.join(folder_path, filename)
-        format_docx(file_path)
-        print(f"Форматирование завершено для: {filename}")
+
+def main():
+    # Вызов функции change_format()
+    change_format('/Users/dmitry.dobrozan/Desktop/case3')
+
+
+def change_format(folder_path):
+    # Проход по всем файлам в папке
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.docx'):
+            file_path = os.path.join(folder_path, filename)
+            format_docx(file_path)
+            print(f"Форматирование завершено для: {filename}")
+
+
+# Инициализация скрипта
+if __name__ == "__main__":
+    main()
